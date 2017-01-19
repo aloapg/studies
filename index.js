2642ac3e0558053581d700694e3b5a31925053ef
Paola <paolagiampa@gmail.com>
2017-01-19 15:45:15 +0100

    Something to test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var spazio =  documen.getElementsByTagName('h2');
+fucntion sommaRicorsiva (x, y) {
+    debugger:
+    if(y > 0) {
+        sommaRicorsiva(x+1, y-1)
+    }else{
+        return document.write(x);
+    }
 
+}
+
+console.log(sommaRicorsiva(10, 10));
